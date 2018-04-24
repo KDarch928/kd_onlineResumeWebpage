7ec064e4aa3e44ecf49cca4473262b39a1060bec
Kayle Drucker <Kayle.Drucker@gmail.com>
2018-04-24 16:45:33 -0500

    updated php script

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -11,13 +11,13 @@
 	$email_from = 'Kayle.Drucker@gmail.com';
 	$to = 'Kayle.Drucker@gmail.com';
 	$email_body = "You have received a new message from: \n 
-	Name: $name\n
-	Email: $visitor_email\n\n
-	Message:\n
-	$message";
+	Name: " . $name . "\n
+	Email: " . $visitor_email . "\n\n
+	Message:\n"
+	$message;
 
-	$headers = "From: $email_from \r\n";
-	$headers .= "Reply-To: $visitor_email \r\n";
+	$headers = "From: " . $email_from . "\r\n";
+	$headers .= "Reply-To: " . $visitor_email . "\r\n";
 
 	mail($to,$email_subject,$email_body,$headers);
 
